ed514aee18b00769165f8576d2c2b1fb8b243bbf
naznin07 <naffect1@gmail.com>
2024-09-08 22:07:47 -0400

    Update App.js

--- App.js
+++ App.js
@@ -10,7 +10,30 @@ export default function App() {
           <Dictionary defaultKeyword="sunset" />
         </main>
         <footer className="App-footer">
-          <small>Coded by SheCodes</small>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/naznin07"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Naznin Sultana
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/naznin07/dictionaryapp"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://dictionary13.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
         </footer>
       </div>
     </div>
